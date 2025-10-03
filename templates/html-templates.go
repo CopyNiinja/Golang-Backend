2d8e8e977b27ed615290a107569c2ca4ec569553
copyniinja <killfyz0.o@gmail.com>
2025-10-03 23:10:21 +0600

    test

---
+++ templates/html-templates.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Faiyaz"
+
+	template := `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hi ` + name + ` </h1>
+  </body>
+</html>
+`
+	fmt.Println(template)
+
+	//run code with this line: go run templates/html-templates.go > index.html
+}
